95706ccf7c6553ff94f00ceb69042c2c1be84068
ttoe05 <terrillt15@gmail.com>
2022-02-21 21:19:37 -0500

    Added files looking at the profiles dataset

---
+++ Cases/I ok Cupid/Ok_Cupid_EDA.R
@@ -0,0 +1,44 @@
+#OK cupid case 1 assignment
+
+library(dplyr)
+library(stringr)
+# Load the dataset address
+getwd()
+setwd("C:/Users/student/Documents/Terrill/Harvard DS/Data_Mining_Business/Harvard_DataMining_Business_Student/Cases/I Ok Cupid"
+)
+df_address <- read.csv('addr.csv')
+#Count of null values per column
+df_address %>% summarise(across(everything(), ~ sum(is.na(.))))
+# Get the dimensions of the dataframe
+dim(df_address)
+# Check locations is a unique column
+unique(df_address$location) %>% length()
+# Return the indexes where the city is null
+na_indxes_city <- which(is.na(df_address$city))
+#iterate over the indexes and split the location into city and state to populate
+# the city column
+
+for (indx in na_indxes_city){
+  loc_split <- strsplit(str_to_title(df_address$location[indx]), split=", ")
+  df_address$city[indx] <- loc_split[[1]][1]
+}
+
+# Return the indexes where the State is null and populate the state column the same as above
+na_indxes_st <- which(is.na(df_address$state))
+
+for (indx in na_indxes_st){
+  loc_split <- strsplit(str_to_title(df_address$location[indx]), split=", ")
+  df_address$state[indx] <- loc_split[[1]][2]
+}
+# Check for unique number of counties
+unique(df_address$county) %>% length()
+# check for unique number of postalcodes
+unique(df_address$postalCode) %>% length()
+# Check for unique cities
+unique(df_address$city) %>% length()
+####################################################################################
+
+df_latlon <- read.csv("Latlon.csv")
+
+
+# Check if postal codes are similar in the data set for counties
